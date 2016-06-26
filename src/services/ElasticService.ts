3593d750d0d7e82821b96cd94423e7559c977df9
Fumarola Fabio (UniCredit) <fabio.fumarola@unicredit.eu>
2016-06-26 12:59:33 +0200

    add

---
+++ src/services/ElasticService.ts
@@ -0,0 +1,44 @@
+﻿module elasticui.services {
+    export class ElasticService {
+        public client;
+        private esFactory;
+        private host;
+        private username;
+        private password;
+
+        static $inject = ['esFactory', 'euiHost', 'euiUser', 'euiPassword'];
+
+        constructor(esFactory, euiHost, euiUser, euiPassword) {
+            this.esFactory = esFactory;
+            this.username = euiUser || '';
+            this.password = euiPassword || '';
+            this.setHost(euiHost);
+        }
+
+        public setHost(host) {
+            if (host === this.host) {
+                return false;
+            }
+            //Add basic auth if username and password provided
+            if (this.username && this.password) {
+                this.host = [
+                    {
+                        host: host,
+                        auth: this.username + ':' + this.password
+                    }
+                ]
+            }
+            else {
+                this.host = host;
+            }
+
+            this.client = this.esFactory({
+                host: this.host,
+                calcDeadTimeout: "flat"
+            });
+
+            return true;
+        }
+    }
+    services.service('es', ElasticService);
+}
